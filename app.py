ac475170e5363ccd6dfbdc6c8ed86c14fa368713
yuting1214 <l501l501l@gmail.com>
2024-03-13 00:42:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
 import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
 
-if __name__ == "__main__":
-    app.run(
-        port=os.getenv("PORT", default=5000),
-        debug=False,
-    )
+@app.route('/')
+def index():
+    port_num = os.getenv("PORT")
+    test_var = os.getenv("TEST")
+    return jsonify({"Choo Choo": f'PORT:{port_num}',
+                   "Test": test_var})
+
+
+if __name__ == '__main__':
+    app.run(debug=False, port=os.getenv("PORT", default=5000))
